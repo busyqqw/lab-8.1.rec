ddffc35bbc9031c8805158533a7c12e79c34faad
busyqqw <matsyta.g@gmail.com>
2023-11-16 18:28:56 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "CppUnitTest.h"
+#include "../дфи 8.1.rec/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char row[] = "nlo";
+			int k = 1;
+			int t = Count(row, 1);
+			Assert::AreEqual(k, t);
+		}
+	};
+}
